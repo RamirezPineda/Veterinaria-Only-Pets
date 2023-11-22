a887c29234442a0947efb0f397fcfcf1abb653d0
= <ramirezpineda.rickyroy@gmail.com>
2023-11-22 08:45:05 -0400

    crud servicios

--- database/migrations/2023_11_21_190428_create_recibos_table.php
+++ database/migrations/2023_11_21_190428_create_recibos_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
             $table->id();
             $table->date('fecha');
             $table->string('concepto',100);
-            $table->integer('monto_cancelado');
-            $table->integer('saldo');
             $table->integer('monto_total');
             $table->unsignedBigInteger('id_administrativo')->nullable();
             $table->foreign('id_administrativo')->references('id')->on('administrativos')->nullOnDelete()->onUpdate('cascade');
