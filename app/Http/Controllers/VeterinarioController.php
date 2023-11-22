82b768a9d3994a77f51239fdc51a111e2452b1c8
= <ramirezpineda.rickyroy@gmail.com>
2023-11-22 08:33:16 -0400

    crud veterinarios

--- app/Http/Controllers/VeterinarioController.php
+++ app/Http/Controllers/VeterinarioController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Persona;
+use App\Models\Usuario;
 use App\Models\Veterinario;
 use Illuminate\Http\Request;
 
@@ -12,7 +14,9 @@ class VeterinarioController extends Controller
      */
     public function index()
     {
-        //
+        $veterinarios = Veterinario::get();
+        $veterinarios->load('persona');
+        return view('veterinarios.index', compact('veterinarios'));
     }
 
     /**
@@ -20,7 +24,7 @@ class VeterinarioController extends Controller
      */
     public function create()
     {
-        //
+        return view('clientes.create');
     }
 
     /**
@@ -28,7 +32,42 @@ class VeterinarioController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nombre' => 'required|string|max:20',
+            'apellido_paterno' => 'required|string|max:20',
+            'apellido_materno' => 'required|string|max:20',
+            'ci' => 'nullable|string|max:10',
+            'direccion' => 'nullable|string|max:254',
+            'email' => 'required|email|max:40',
+            'fecha_de_nacimiento' => 'nullable|date',
+            'sexo' => 'nullable|in:M,F',
+        ]);
+        
+        $persona = Persona::create([
+            'nombre'           => $request->nombre,
+            'apellido_paterno' => $request->apellido_paterno,
+            'apellido_materno' => $request->apellido_materno,
+            'email'            => $request->email,
+            'direccion'        => $request->direccion,
+            'fecha_de_nacimiento' => $request->fecha_de_nacimiento,
+            'sexo'             => $request->sexo,
+            'ci'               => $request->ci,
+        ]);
+
+        Veterinario::create([
+            'id'        => $persona->id,
+            'profesion' => $request->profesion,
+            'id_servicio' => $request->servicio,
+        ]);
+
+        Usuario::create([
+            'nombre_usuario' => $request->email,
+            'password'       => bcrypt($request->ci),
+            'enable'         => '1',
+            'id_persona'     => $persona->id,
+        ]);
+
+        return redirect()->route('veterinarios.index');
     }
 
     /**
@@ -36,7 +75,7 @@ class VeterinarioController extends Controller
      */
     public function show(Veterinario $veterinario)
     {
-        //
+        return view('administrativo.show', compact('veterinario'));
     }
 
     /**
@@ -44,15 +83,55 @@ class VeterinarioController extends Controller
      */
     public function edit(Veterinario $veterinario)
     {
-        //
+        return view('clientes.edit', compact('veterinario'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Veterinario $veterinario)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'nombre' => 'required|string|max:20',
+            'apellido_paterno' => 'required|string|max:20',
+            'apellido_materno' => 'required|string|max:20',
+            'ci' => 'nullable|string|max:10',
+            'direccion' => 'nullable|string|max:254',
+            'email' => 'required|email|max:40',
+            'fecha_de_nacimiento' => 'nullable|date',
+            'sexo' => 'nullable|in:M,F',
+        ]);
+
+        $persona = Persona::findOrFail($id);
+        $veterinario = Veterinario::findOrFail($id);
+
+        $veterinario->profesion = $request->profesion;
+        $veterinario->id_servicio = $request->servicio;
+        $data = ([
+            'nombre'              => $request->nombre,
+            'apellido_paterno'    => $request->apellido_paterno,
+            'apellido_materno'    => $request->apellido_materno,
+            'email'               => $request->email,
+            'ci'                  => $request->ci,
+            'direccion'           => $request->direccion,
+            'fecha_de_nacimiento' => $request->fecha_de_nacimiento,
+            'sexo'                => $request->sexo,
+        ]);
+
+        $persona->update($data);
+        $veterinario->update();
+
+        return redirect()->route('veterinarios.index');
+    }
+
+    
+    public function datas($id)
+    {
+        $veterinario = Veterinario::find($id);
+        $veterinario->load('turno');
+        $veterinario->load('persona');
+        $veterinario->persona->load('telefonos');
+        return $veterinario;
     }
 
     /**
