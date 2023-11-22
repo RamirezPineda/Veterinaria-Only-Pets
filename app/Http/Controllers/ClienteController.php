830176048a3a5ee39997b34107edfe6e885ddf2a
= <ramirezpineda.rickyroy@gmail.com>
2023-11-22 08:10:43 -0400

    crud clientes

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Cliente;
+use App\Models\Persona;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
 
 class ClienteController extends Controller
@@ -12,7 +14,9 @@ class ClienteController extends Controller
      */
     public function index()
     {
-        //
+        $clientes = Cliente::all();
+        $clientes->load('persona');
+        return view('clientes.index', compact('clientes'));
     }
 
     /**
@@ -20,7 +24,7 @@ class ClienteController extends Controller
      */
     public function create()
     {
-        //
+        return view('clientes.create');
     }
 
     /**
@@ -28,7 +32,40 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nombre' => 'required|string|max:20',
+            'apellido_paterno' => 'required|string|max:20',
+            'apellido_materno' => 'required|string|max:20',
+            'ci' => 'nullable|string|max:10|unique:personas,ci',
+            'direccion' => 'nullable|string|max:254',
+            'email' => 'required|email|max:40|unique:personas,email',
+            'fecha_de_nacimiento' => 'nullable|date',
+            'sexo' => 'nullable|in:M,F',
+        ]);
+
+        $persona = Persona::create([
+            'nombre' => $request->input('nombre'),
+            'apellido_paterno' => $request->input('apellido_paterno'),
+            'apellido_materno' => $request->input('apellido_materno'),
+            'ci' => $request->input('ci'),
+            'direccion' => $request->input('direccion'),
+            'email' => $request->input('email'),
+            'fecha_de_nacimiento' => $request->input('fecha_de_nacimiento'),
+            'sexo' => $request->input('sexo'),
+        ]);
+
+        $cliente = Cliente::create([
+            'id' => $persona->id,
+        ]);
+
+        $usuario = Usuario::create([
+            'nombre_usuario' => $request->input('email'),
+            'password' => bcrypt($request->input('ci')),
+            'enable' => '1',
+            'id_persona' => $persona->id,
+        ]);
+
+        return redirect()->route('clientes.index')->with('success', 'Cliente creado con éxito.');
     }
 
     /**
@@ -36,7 +73,7 @@ class ClienteController extends Controller
      */
     public function show(Cliente $cliente)
     {
-        //
+        return view('clientes.show', compact('cliente'));
     }
 
     /**
@@ -44,7 +81,7 @@ class ClienteController extends Controller
      */
     public function edit(Cliente $cliente)
     {
-        //
+        return view('clientes.edit', compact('cliente'));
     }
 
     /**
@@ -52,7 +89,29 @@ class ClienteController extends Controller
      */
     public function update(Request $request, Cliente $cliente)
     {
-        //
+        $request->validate([
+            'nombre' => 'required|string|max:20',
+            'apellido_paterno' => 'required|string|max:20',
+            'apellido_materno' => 'required|string|max:20',
+            'ci' => 'nullable|string|max:10',
+            'direccion' => 'nullable|string|max:254',
+            'email' => 'required|email|max:40',
+            'fecha_de_nacimiento' => 'nullable|date',
+            'sexo' => 'nullable|in:M,F',
+        ]);
+
+        $cliente->persona->update([
+            'nombre' => $request->input('nombre'),
+            'apellido_paterno' => $request->input('apellido_paterno'),
+            'apellido_materno' => $request->input('apellido_materno'),
+            'ci' => $request->input('ci'),
+            'direccion' => $request->input('direccion'),
+            'email' => $request->input('email'),
+            'fecha_de_nacimiento' => $request->input('fecha_de_nacimiento'),
+            'sexo' => $request->input('sexo'),
+        ]);
+
+        return redirect()->route('clientes.index')->with('success', 'Cliente actualizado con éxito.');
     }
 
     /**
@@ -60,6 +119,8 @@ class ClienteController extends Controller
      */
     public function destroy(Cliente $cliente)
     {
-        //
+        $cliente->delete();
+        return redirect()->route('clientes.index')->with('success', 'Cliente eliminado con éxito.');
     }
 }
+
--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Persona extends Model
 {
     use HasFactory;
+
+    protected $table = 'personas';
+
+    protected $fillable = [
+        'nombre',
+        'apellido_paterno',
+        'apellido_materno',
+        'ci',
+        'direccion',
+        'email',
+        'fecha_de_nacimiento',
+        'sexo',
+    ];
+
+    public function cliente()
+    {
+        return $this->hasOne(Cliente::class, 'id');
+    }
+
+    public function usuario()
+    {
+        return $this->hasOne(Usuario::class, 'id_persona');
+    }
 }
